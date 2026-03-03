ee356fc1835b43fee5056a4ec1889afb6d384e79
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-04 00:40:56 +0100

    Create MedicationSafetyApplication.java

---
+++ src/main/java/com/shotaroi/medsafety/MedicationSafetyApplication.java
@@ -0,0 +1,12 @@
+package com.shotaroi.medsafety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicationSafetyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicationSafetyApplication.class, args);
+    }
+}
